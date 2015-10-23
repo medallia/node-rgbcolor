2366a082f9f996bbc233f81eb130b5ca624a7c81
Denise Fernandez <denise@medallia.com>
2015-10-23 19:04:01 -0300

    Add HSL + darken/lighten support

--- index.js
+++ index.js
@@ -298,4 +298,76 @@ module.exports = function(color_string) {
 
     }
 
+    /**
+     * Converts an RGB color value to HSL. Conversion formula
+     * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
+     * Assumes r, g, and b are contained in the set [0, 255] and
+     * returns h, s, and l are contained in the set [0..1].
+     *
+     * @param   Number  r       The red color value
+     * @param   Number  g       The green color value
+     * @param   Number  b       The blue color value
+     * @return  Array           The HSL representation
+     */
+    this.toHsl = function () {
+        var r = this.r,
+            g = this.g,
+            b = this.b;
+
+        r /= 255, g /= 255, b /= 255;
+
+        var max = Math.max(r, g, b);
+        var min = Math.min(r, g, b);
+        var h, s, l = (max + min) / 2;
+
+        if (max == min) {
+            h = s = 0; // achromatic
+        } else {
+            var d = max - min;
+            s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+            switch (max) {
+                case r:
+                    h = (g - b) / d + (g < b ? 6 : 0);
+                    break;
+                case g:
+                    h = (b - r) / d + 2;
+                    break;
+                case b:
+                    h = (r - g) / d + 4;
+                    break;
+            }
+            h /= 6;
+        }
+
+        return [h, s, l];
+    };
+
+    /*
+     * Returns a new RGBColor that is a given absolute amount darker than the receiver.
+     * Example:
+     *          var darkerRed = (new RGBColor('#FF0000')).darken(0.2);
+     */
+    this.darken = function (number) {
+        var amount = parseFloat(number);
+
+        var hsl = this.toHsl();
+
+        hsl[2] -= amount;
+
+        return new RGBColor('hsl(' + Math.round(hsl[0] * 360) + ',' + Math.round(hsl[1] * 100) + '%,' + Math.round(hsl[2] * 100) + '%)');
+    };
+
+    /**
+     * Opposite of darken
+     */
+    this.lighten = function (number) {
+        var amount = parseFloat(number);
+
+        var hsl = this.toHsl();
+
+        hsl[2] += amount;
+
+        return new RGBColor('hsl(' + Math.round(hsl[0] * 360) + ',' + Math.round(hsl[1] * 100) + '%,' + Math.round(hsl[2] * 100) + '%)');
+    };
+
 }
